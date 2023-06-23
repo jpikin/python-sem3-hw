624e1274f009a8ddf336a522284b98a9902f267a
j.pikin <pikin@bk.ru>
2023-06-23 10:23:04 +0500

    task 2 added 2 var

--- task2.py
+++ task2.py
@@ -3,5 +3,19 @@
 
 list_1 = [1, 2, 3, 4, 5]
 k = 6
+
+# 1 Вариант
 closest_num = min(list_1, key=lambda x: abs(x - k))
 print(closest_num)
+
+
+# 2 Вариант
+temp = abs(list_1[0]-k)
+closest_num2 = 0
+
+for i in list_1:
+    if abs(i - k) < temp:
+        closest_num2 = i
+        temp = abs(i - k)
+
+print(closest_num2)
